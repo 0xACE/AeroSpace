fc9f5f8adbeba07edf24c08086618c153f3d273f
0xACE <0xACED@gmail.com>
2026-05-10 21:42:33 +0200

    wip

---
+++ Casks/aerospace.rb
@@ -0,0 +1,36 @@
+cask "aerospace" do # version and sha256 are updated by .github/workflows/release.yml
+  version "0.0.0-placeholder"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/0xACE/AeroSpace/releases/download/v#{version}/AeroSpace-v#{version}.zip"
+  name "AeroSpace"
+  desc "AeroSpace is an i3-like tiling window manager for macOS"
+  homepage "https://github.com/0xACE/AeroSpace"
+  conflicts_with cask: "nikitabobko/tap/aerospace"
+
+  depends_on macos: ">= :ventura"
+
+  postflight do
+    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/AeroSpace-v#{version}/bin/aerospace"
+    system "xattr", "-d", "com.apple.quarantine", "#{appdir}/AeroSpace.app"
+  end
+
+  app "AeroSpace-v#{version}/AeroSpace.app"
+  binary "AeroSpace-v#{version}/bin/aerospace"
+
+  binary "AeroSpace-v#{version}/shell-completion/zsh/_aerospace",
+      target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_aerospace"
+  binary "AeroSpace-v#{version}/shell-completion/bash/aerospace",
+      target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/aerospace"
+  binary "AeroSpace-v#{version}/shell-completion/fish/aerospace.fish",
+      target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/aerospace.fish"
+
+  Dir["#{staged_path}/AeroSpace-v#{version}/manpage/*"].each { |man| manpage man }
+
+  uninstall quit: "bobko.AeroSpace"
+
+  zap trash: [
+    "~/Library/Application Support/AeroSpace",
+    "~/Library/Preferences/bobko.AeroSpace.plist",
+  ]
+end
